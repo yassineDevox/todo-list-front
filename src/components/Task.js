d3a5194214abb828d4783707168043bb38798f38
yassineDevox <yassine.rassy1@gmail.com>
2022-01-14 12:13:45 +0100

    fix issues

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,21 +2,21 @@
 export default function Task(props) {
 
 
-    const handleClick = ()=>{
-        if(window.confirm("Are you sure ?"))
-        props.deleteTaskById(props.id)
+    const handleClick = () => {
+        if (window.confirm("Are you sure ?"))
+            props.deleteTaskById(props.id)
     }
     console.log(props);//afficher la liste des attributs(props)
     return (
         <li className="list-group-item listup">
             <span className="fs-4 tasklist">{props.title}
-            <br /> desc : {props.desc}</span>
-            <button 
+                <br /> desc : {props.desc}</span>
+            <button
                 onClick={handleClick}
-            className="btn btn-danger float-end m-3" onclick="deleteTask(this)">
+                className="btn btn-danger float-end m-3" >
                 <i className="fas fa-trash" />
             </button>
-            <button className="btn btn-success float-end m-3" onclick="editTask(this)">
+            <button className="btn btn-success float-end m-3">
                 <i className="fas fa-edit" />
             </button>
         </li>
