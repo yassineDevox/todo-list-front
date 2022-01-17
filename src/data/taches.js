ad797ccf28ff00716e33c02305a3d5bb8ad61934
yassineDevox <yassine.rassy1@gmail.com>
2022-01-17 13:21:48 +0100

    fix dumb issues

--- src/data/taches.js
+++ src/data/taches.js
@@ -1,8 +1,4 @@
-import { Tache } from "../models/tache";
 
 export const LIST_TASK = [
-    // new Tache(1,"title 1","description 1"),
-    // new Tache(2,"title 2","description 2"),
-    // new Tache(3,"title 3","description 3"),
     
 ]
