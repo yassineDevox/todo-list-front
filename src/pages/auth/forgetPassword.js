dab7c57de48d18415f1d60ce31090b353552d76e
yassineDevox <yassine.rassy1@gmail.com>
2022-02-16 01:32:17 +0100

    forget password fix design issues in progress

--- src/pages/auth/forgetPassword.js
+++ src/pages/auth/forgetPassword.js
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "./../../assets/img/loginLogo.svg";
 import "./../../assets/style/forgetPassword.css";
 
 export const ForgetPassword = () => {
-  
   const [error, setError] = useState(false);
   const emailRef = useRef("");
 
@@ -18,36 +18,41 @@ export const ForgetPassword = () => {
 
   const handleFocusInput = () => setError(false);
   return (
-    <div className="form-container">
-      <div className="form-heading">
-        <img src={logo} alt="logo" />
-        <h4>Enter the email associated with your account</h4>
-      </div>
-      <div className="form-body">
-        <form onSubmit={handleLoginSubmit}>
-          <input
-            ref={emailRef}
-            onFocus={handleFocusInput}
-            className={!error ? "" : "input-error"}
-            type="text"
-            placeholder="Email address"
-            autoComplete={false}
-          />
+    <>
+     <p className="back-login">
+      <Link to="/">Back to login</Link>
+     </p>
+      <div className="form-container">
+        <div className="form-heading">
+          <img src={logo} alt="logo" height={25} />
+          <h4>Enter the email associated with your account</h4>
+        </div>
+        <div className="form-body">
+          <form onSubmit={handleLoginSubmit}>
+            <input
+              ref={emailRef}
+              onFocus={handleFocusInput}
+              className={!error ? "" : "input-error"}
+              type="text"
+              placeholder="Email address"
+              autoComplete={false}
+            />
 
-          <span className={!error ? "" : "msg-error"}>
-            Please enter your email !
+            <span className={!error ? "" : "msg-error"}>
+              Please enter your email !
+            </span>
+
+            <br />
+            <button type="submit">Reset Password</button>
+          </form>
+        </div>
+        <div className="form-footer">
+          <span>
+            If you have any trouble resetting your password, contact us at
+            <a href=""> hello@domycode.co</a>
           </span>
-
-          <br />
-          <button type="submit">Reset Password</button>
-        </form>
+        </div>
       </div>
-      <div className="form-footer">
-        <span>
-          If you have any trouble resetting your password, contact us at 
-          <a href=""> hello@domycode.co</a>
-        </span>
-      </div>
-    </div>
+    </>
   );
 };
