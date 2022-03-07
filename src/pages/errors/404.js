657bdb14e22825541442f3b6d24e64c1d9b42cb7
yassineDevox <yassine.rassy1@gmail.com>
2022-03-07 12:52:08 +0100

    done with error 404 impl

--- src/pages/errors/404.js
+++ src/pages/errors/404.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import Page404 from "./../../assets/images/404.svg"
+import React from "react";
+import Page404 from "assets/images/404.svg";
+import { useNavigate } from "react-router-dom";
 
 export const Error404 = () => {
+  const navTo = useNavigate();
   return (
-    <div className='d-flex justify-content-center align-items-center vh-100'>
-        <img src={Page404} alt="not found page" />    
+    <div className="d-flex flex-column gap-3 justify-content-center align-items-center vh-100">
+      <img src={Page404} alt="not found page" width="500" height="400" />
+      <button className="btn btn-success" onClick={() => navTo("/todo")}>
+        Back Home 😇
+      </button>
     </div>
-  )
-}
-
+  );
+};
